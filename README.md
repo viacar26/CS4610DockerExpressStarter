9506c376f56f0e25814f0a27ced844fa1a6fc696
Joseph Ditton <jditton.atomic@gmail.com>
2025-02-04 09:21:49 -0700

    adds docker setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# CS 4610 Express React Starter App
+This simple application just gives a basic React+Vite/Express setup. It is meant to be a starting point for simple projects but is lacking many features that a full-fledged application would have.
+In the future you will be given a more complete application to work with.
+
+## Getting Started
+1. Fork/Clone this repository
+2. Run `npm install` in the server directory
+3. Run `npm install` in the client directory
+4. Create a file called `.env` in the server directory and copy the contents of `.env.example` into it. DON'T RENAME THE EXAMPLE FILE.
+5. Run `npm run dev` in the server directory
+6. Run `npm run dev` in the client directory
+7. Open your browser to `http://localhost:3000`
+Make sure are running the server and client in separate terminals.
+
+Happy coding!
---
+++ server/index.ts
@@ -0,0 +1,60 @@
+import express from 'express';
+import { config } from 'dotenv';
+import http from 'http';
+
+
+// load environment variables
+config();
+
+const app = express();
+
+// slightly modified version of the code we wrote in class.
+// we wrap the express app in a node http server so that we can
+// expose the server to socket.io later on.
+const server = http.createServer(app);
+const port = parseInt(process.env.PORT || '3000');
+
+
+// a simple middleware the redirects
+// to the asset server if the request
+// path contains a dot. We use the dot
+// to differentiate between asset requests
+// and normal requests because file names have
+// dots in them.
+app.use((req, res, next) => {
+  if (req.path.includes(".")) {
+    res.redirect(process.env.ASSET_URL + req.path);
+    return;
+  }
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send(`
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <link rel="icon" type="image/svg+xml" href="${process.env.ASSET_URL}/vite.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>React-Express Starter App</title>
+        <script type="module">
+          import RefreshRuntime from '${process.env.ASSET_URL}/@react-refresh'
+          RefreshRuntime.injectIntoGlobalHook(window)
+          window.$RefreshReg$ = () => {}
+          window.$RefreshSig$ = () => (type) => type
+          window.__vite_plugin_react_preamble_installed__ = true
+        </script>
+        <script type="module" src="${process.env.ASSET_URL}/@vite/client"></script>
+        </head>
+        <body>
+        <div id="root"></div>
+        <script type="module" src="${process.env.ASSET_URL}/src/main.tsx"></script>
+      </body>
+    </html>
+    `);
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
