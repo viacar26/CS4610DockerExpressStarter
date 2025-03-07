08851216fa0741953fd2b8fa23d42771b5f9a79e
Vikki Ballard <A00576416@usu.edu>
2025-03-06 18:11:23 -0700

    cleaned up login code

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -9,15 +9,12 @@ const Login: React.FC = () => {
 
   async function checkUsername(username: string) {
     try {
-      console.log("Checking user in checkUsername:", username);
       const response = await fetch(`/api/user/${username}`);
-      console.log("Response:", response);
       if (!response.ok) {
         console.log("User not found", response.status);
         throw new Error("User not found");
       }
       const data = await response.json();
-      console.log("API Response:", data);
       // ensure username is not empty 
       if (username.length === 0) {      
         setError("Please enter a username");
@@ -32,42 +29,6 @@ const Login: React.FC = () => {
       return false;
     }
   }
-
-    // const loginUser = async () => {
-    //   if (username.length === 0) {      
-    //     setError("Please enter a username");
-    //     return;
-    //   }
-
-      // const userTaken = await checkUsername(username);
-      // console.log("User taken:", userTaken);
-      // if (userTaken) {
-      //   // Log user in
-      //   navigate("/tutor");
-      //   return;
-      // }
-
-      // Create a new user in database
-  //     try {
-  //       const response = await fetch("/api/user", {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json"
-  //         },
-  //         body: JSON.stringify({ username })
-  //       });
-
-  //       if (!response.ok) {
-  //         throw new Error("Error creating user");
-  //       }
-
-  //     setError("");
-  //     navigate("/tutor");
-  //     } catch (error) {
-  //       console.error("Error creating user: ", error);
-  //       setError("Error creating user. Please try again.");
-  //     }
-  // };
 
   return (  
     <>
